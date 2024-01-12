398dfd4557393db8bde609a885296529a7fea301
Jamez213 <dphillipjames@yahoo.com>
2024-01-12 22:29:55 +0800

    index(Complete) style(Complete) js(need for optimization)

---
+++ script.js
@@ -0,0 +1,48 @@
+document.getElementById('Red').addEventListener('click',function() {
+    document.body.style.backgroundColor = '#FF0000' // Change color to Red
+});
+
+document.getElementById('Blue').addEventListener('click',function() {
+    document.body.style.backgroundColor = '#0000FF' //Change color to Blue
+});
+
+document.getElementById('Green').addEventListener('click',function() {
+    document.body.style.backgroundColor = '#00FF00' //Change color to Green
+});
+
+document.getElementById('Violet').addEventListener('click',function() {
+    document.body.style.backgroundColor = '#9400D3' //Change color to Violet
+});
+
+document.getElementById('Black').addEventListener('click',function() {
+    document.body.style.backgroundColor = '#000000' //Change color to Black
+});
+
+document.getElementById('White').addEventListener('click',function() {
+    document.body.style.backgroundColor = '#FFFFFF' //Change color to White
+});
+
+
+document.getElementById('Orange').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#FFA500'; //Change color to Orange
+});
+
+document.getElementById('Gray').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#808080'; //Change color to Gray
+});
+
+document.getElementById('Brown').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#A52A2A'; //Change color to Brown
+});
+
+document.getElementById('Yellow').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#FFFF00'; //Change color to Yellow
+});
+
+document.getElementById('Pink').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#FFC0CB'; //Change color to Pink
+});
+
+document.getElementById('Cyan').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#00FFFF'; //Change color to Cyan
+});

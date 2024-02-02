0c6e490f09387d8513aaa4247b6ad96db30f3624
Jamez213 <dphillipjames@yahoo.com>
2024-02-03 02:14:11 +0800

    Opimization of codes

--- script.js
+++ script.js
@@ -1,48 +1,20 @@
-document.getElementById('Red').addEventListener('click',function() {
-    document.body.style.backgroundColor = '#FF0000' // Change color to Red
-});
+const colors = {
+    'Red': '#FF0000',
+    'Blue': '#0000FF',
+    'Green': '#00FF00',
+    'Violet': '#9400D3',
+    'Black': '#000000',
+    'White': '#FFFFFF',
+    'Orange': '#FFA500',
+    'Gray': '#808080',
+    'Brown': '#A52A2A',
+    'Yellow': '#FFFF00',
+    'Pink': '#FFC0CB',
+    'Cyan': '#00FFFF'
+};
 
-document.getElementById('Blue').addEventListener('click',function() {
-    document.body.style.backgroundColor = '#0000FF' //Change color to Blue
-});
-
-document.getElementById('Green').addEventListener('click',function() {
-    document.body.style.backgroundColor = '#00FF00' //Change color to Green
-});
-
-document.getElementById('Violet').addEventListener('click',function() {
-    document.body.style.backgroundColor = '#9400D3' //Change color to Violet
-});
-
-document.getElementById('Black').addEventListener('click',function() {
-    document.body.style.backgroundColor = '#000000' //Change color to Black
-});
-
-document.getElementById('White').addEventListener('click',function() {
-    document.body.style.backgroundColor = '#FFFFFF' //Change color to White
-});
-
-
-document.getElementById('Orange').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#FFA500'; //Change color to Orange
-});
-
-document.getElementById('Gray').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#808080'; //Change color to Gray
-});
-
-document.getElementById('Brown').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#A52A2A'; //Change color to Brown
-});
-
-document.getElementById('Yellow').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#FFFF00'; //Change color to Yellow
-});
-
-document.getElementById('Pink').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#FFC0CB'; //Change color to Pink
-});
-
-document.getElementById('Cyan').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#00FFFF'; //Change color to Cyan
-});
+for (const color in colors) {
+    document.getElementById(color).addEventListener('click', function() {
+        document.body.style.backgroundColor = colors[color];
+    });
+}
